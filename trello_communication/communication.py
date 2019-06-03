c1d2471eae1de3b4a632cfd779cbfe55817e6906
smol <nekit.smol@gmail.com>
2019-06-03 14:57:59 +0000

    Added another parsing option

--- trello_communication/communication.py
+++ trello_communication/communication.py
@@ -1,6 +1,8 @@
 from flask import redirect
 import requests
 import typing
+import json
+import re
 from entity.board import Board
 from entity.list import List
 from entity.card import Card
@@ -13,6 +15,7 @@ class _Communication:
         self.base_url = "https://api.trello.com/1"
         self.token = ""
         self.user_id = ""
+        self.is_in_name = False
 
     def get_boards(self) -> typing.List[Board]:
         return requests.request("GET", self.base_url + "/members/" + str(self.user_id) + "/boards").content
@@ -36,14 +39,30 @@ class _Communication:
         return requests.request("GET", self.base_url + "/lists/" + list_id + "/cards").content
 
     def get_story_points_from_card(self, card_id: str):
-        return requests.request("GET", self.base_url + "/cards/" + card_id + "/pluginData").content
+        res = {}
+        if self.is_in_name:
+            storypoints = requests.request("GET", self.base_url + "/cards/" + card_id).content
+            card_dict = json.loads(storypoints)
+            name: str = card_dict['name']
+
+            if '(' in name and ')' in name:
+                point = re.search(r'\(\d+\)', name).group(0)
+
+                if point:
+                    res['points'] = int(point[1: -1])
+
+        else:
+            storypoints = requests.request("GET", self.base_url + "/cards/" + card_id + "/pluginData").content
+            point_list = json.loads(storypoints)
+
+            if len(point_list) != 0:
+                point_dict = [x for x in point_list if x['idPlugin'] == '59d4ef8cfea15a55b0086614'][0]
+                res['points'] = int(json.loads(point_dict['value'])['points'])
+
+        return json.dumps(res)
 
     def authorize(self, auth_url):
         return redirect(auth_url, 302)
 
 
 communication = _Communication()
-
-
-
-
